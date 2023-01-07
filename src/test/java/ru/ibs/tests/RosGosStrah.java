c95c3640285587c2ef08992cea93fe6c24567b7c
reeffeer <Vfcnthgthytn1>
2023-01-07 12:14:25 +0300

    Fixed some scripts

--- src/test/java/ru/ibs/tests/RosGosStrah.java
+++ src/test/java/ru/ibs/tests/RosGosStrah.java
@@ -1,68 +1,122 @@
 package ru.ibs.tests;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
 public class RosGosStrah {
+    WebDriver driver;
+
+    @Before
+    public void before() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.get("http://www.rgs.ru");
+    }
 
     @Test
     public void test() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+            //Click on companies
+            WebElement companies = driver.findElement(By.xpath
+                    ("//*[@id=\"__layout\"]/div/header/div/div/section[1]/div/nav/ul/li[2]/a"));
+            companies.click();
 
-        driver.get("http://www.rgs.ru");
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            //Find iFrame and close
+            driver.switchTo().frame(driver.findElement(By.xpath("//iframe[contains(@id, 'fl-616371')]")));
+            driver.findElement(By.xpath("//div [@data-fl-track='click-close-login']")).click();
+            driver.switchTo().defaultContent();
 
-        WebDriverWait wait = new WebDriverWait(driver, 15);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("fl-616371"))).click();
-        WebElement frame = driver.findElement(By.id("fl-616371"));
+            //Click on Здоровье
+            WebElement health = driver.findElement(By.cssSelector
+                    ("#__layout > div > header > div > div " +
+                            "> section.section.header-section.header-section--space-between.mobileHidden.section-categories " +
+                            "> div > div > div.category-list > div > ul > li:nth-child(2)"));
+            health = (new WebDriverWait(driver, 10))
+                    .until(ExpectedConditions.elementToBeClickable(health));
+            health.click();
 
-        WebElement close = frame.findElement(By.xpath("/html/body/div/div[1]/div[1]/div"));
-        close.click();
+            //Click on ДМС
+            WebElement dms = driver.findElement(By.xpath(
+                    "//*[@id=\"__layout\"]/div/header/div/div[2]/div[2]/div/div/div/section/div/div[2]/div[1]/div/div/ul/li[3]/a"
+            ));
+            dms = (new WebDriverWait(driver, 10))
+                    .until(ExpectedConditions.elementToBeClickable(dms));
+            dms.click();
 
-        driver.switchTo().defaultContent();
+            //Click on button
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            scroll(driver.findElement(By.xpath("//*[@id=\"__layout\"]/div/main/div[1]/div/div[1]/section/div/div")), 0, 250);
+            WebElement button = driver.findElement(By.xpath("//*[@id=\"__layout\"]/div/main/div[1]/div/div[1]/section/div/div/div/div[4]/button"));
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            button.click();
 
-        WebElement forCompanies = driver.findElement(By.xpath("/html/body/div[1]/div/div/header/div/div/section[1]/div/nav/ul/li[2]/a"));
-        forCompanies.click();
+            //Fill the
+            WebElement name = driver.findElement(By.xpath(
+                    "//*[@id=\"__layout\"]/div/main/div[1]/div/div[4]/section/div/div/div/div/div[1]/div[2]/div/form/div/div[1]/div/div/div/input"));
+            name.sendKeys("Апре Проим Куваф");
 
-        WebElement health = driver.findElement(By.xpath("/html/body/div[1]/div/div/header/div/div/section[2]/div/div/div[1]/div/ul/li[2]/span"));
-        health.click();
+            driver.findElement(By.xpath(
+                    "//*[@id=\"__layout\"]/div/main/div[1]/div/div[4]/section/div/div/div/div/div[1]/div[2]/div/form/div/div[2]/div/div/div/input"
+            )).sendKeys("9110362390");
 
-        WebElement dms = driver.findElement(By.xpath("/html/body/div[1]/div/div/main/div[1]/div/div[2]/section/div/div/div/div/div[1]/div/div[1]/div/a"));
-        dms.click();
+            WebElement email = driver.findElement(By.xpath(
+                    "//*[@id=\"__layout\"]/div/main/div[1]/div/div[4]/section/div/div/div/div/div[1]/div[2]/div/form/div/div[3]/div/div/div/input"));
+            email.sendKeys("qwertyqwerty");
 
-        driver.findElement(By.xpath("/html/body/div[1]/div/div/main/div[1]/div/div[1]/section/div/div/div/div[2]/h1")).isDisplayed();
+//            TODO address doesn't work
+//            driver.findElement(By.xpath(
+//                    "//*[@id=\"__layout\"]/div/main/div[1]/div/div[4]/section/div/div/div/div/div[1]/div[2]/div/form/div/div[4]/div/div/div/div/div/div/input"
+//            )).sendKeys("Санкт-Петербург, ул. Горького, д. 12");
+//
+//            scroll(driver.findElement(By.xpath("//input[@type= 'checkbox']")),0, 200 );
+//            driver.findElement(By.xpath("//input[@type= 'checkbox']//..")).click();
 
-        WebElement send = driver.findElement(By.xpath("/html/body/div[1]/div/div/main/div[1]/div/div[1]/section/div/div/div/div[4]/button"));
-        send.click();
+        //Click on checkbox
+        //TODO Doesn't work
+//        WebElement checkbox = driver.findElement(By.xpath(
+//                "//*[@id=\"accept\"]/div/label"
+//        ));
+//        checkbox.click();
 
-        driver.findElement(By.xpath("/html/body/div[1]/div/div/main/div[1]/div/div[4]/section/div/div/div/div/div[1]")).isDisplayed();
+//        driver.findElement(By.xpath(
+//                "//*[@id=\"__layout\"]/div/main/div[1]/div/div[4]/section/div/div/div/div/div[1]/div[2]/div/form/div/div[6]/div/button"
+//        )).click();
+    }
 
-        WebElement name = driver.findElement(By.xpath("/html/body/div[1]/div/div/main/div[1]/div/div[4]/section/div/div/div/div/div[1]/div[2]/div/form/div/div[1]/div/div/div/input"));
-        name.sendKeys("Иванов Иван Иванович");
+    public WebElement scroll(WebElement element, int x, int y) {
+        String code = "window.scroll(" + (element.getLocation().x + x) +
+                ", " + (element.getLocation().y + y) + ");";
+        ((JavascriptExecutor) driver).executeScript(code, element, x, y);
+        return element;
+    }
 
-        WebElement phone = driver.findElement(By.xpath("/html/body/div[1]/div/div/main/div[1]/div/div[4]/section/div/div/div/div/div[1]/div[2]/div/form/div/div[2]/div/div/div/input"));
-        phone.sendKeys("+79114550923");
-
-        WebElement email = driver.findElement(By.xpath("/html/body/div[1]/div/div/main/div[1]/div/div[4]/section/div/div/div/div/div[1]/div[2]/div/form/div/div[3]/div/div/div/input"));
-        email.sendKeys("qwertyqwerty");
-        driver.findElement(By.xpath("/html/body/div[1]/div/div/main/div[1]/div/div[4]/section/div/div/div/div/div[1]/div[2]/div/form/div/div[3]/div/div/span")).isDisplayed();
-
-        WebElement address = driver.findElement(By.xpath("/html/body/div[1]/div/div/main/div[1]/div/div[4]/section/div/div/div/div/div[1]/div[2]/div/form/div/div[4]/div/div/div/div/div/div/input"));
-        address.sendKeys("СПб, Устинова ул., 23, 44");
-
-        WebElement agree = driver.findElement(By.xpath("/html/body/div[1]/div/div/main/div[1]/div/div[4]/section/div/div/div/div/div[1]/div[2]/div/form/div/div[5]/div/div/label/input"));
-        agree.click();
-
-        email.sendKeys("jhgf@bk.ru");
-
-        WebElement buttonOrder = driver.findElement(By.xpath("/html/body/div[1]/div/div/main/div[1]/div/div[4]/section/div/div/div/div/div[1]/div[2]/div/form/div/div[6]/div/button"));
-        buttonOrder.click();
-
+    @After
+    public void after() {
         driver.quit();
     }
 }
